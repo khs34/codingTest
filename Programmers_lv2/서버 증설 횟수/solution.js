d70d7a74d0676144ff1dc5db9bfc7f869db6b09a
hskim <hyunsu9400@gmail.com>
2025-03-22 20:43:20 +0900

    코테(서버증설횟수)

---
+++ Programmers_lv2/서버 증설 횟수/solution.js
@@ -0,0 +1,51 @@
+function solution(players, m, k) {
+    const servers = m;
+    const operTime = k;
+    
+    let answer = 0;
+    let map = new Map();
+    // 증설된 서버 수
+    let added = 0;
+
+    for(let i = 0; i < players.length; i++){
+        // 시간대 별 게임 이용자 수(i = 시간)
+        let player = players[i];
+        
+        // 서버 증설 및 시간대 저장
+        if (player-(servers*added) >= servers) {
+            answer += Math.floor(player/servers-added);
+            added += Math.floor(player/servers-added);
+            map.set(i, [operTime, added]);
+        }
+        // 시간대 별 서버 남은 시간 체크
+        for (let j = 0; j < i; j++){
+            let value = map.get(j);
+
+            if(map.has(j)){
+                value[0] -=1 ;
+                map.set(j, value);
+                
+                if(value[0] == 0) {
+                    added -= value[1];
+                }
+            }
+        }
+    }
+    console.log(answer);
+}
+
+
+const players1 = [0, 2, 3, 3, 1, 2, 0, 0, 0, 0, 4, 2, 0, 6, 0, 4, 2, 13, 3, 5, 10, 0, 1, 5]	
+const m1 = 3	
+const k1 = 5	
+solution(players1, m1, k1) //7
+
+const players2 = [0, 0, 0, 10, 0, 12, 0, 15, 0, 1, 0, 1, 0, 0, 0, 5, 0, 0, 11, 0, 8, 0, 0, 0]	
+const m2 = 5	
+const k2 = 1	
+solution(players2, m2, k2) //11
+
+const players3 = [0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 5, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1]	
+const m3 = 1	
+const k3 = 1	
+solution(players3, m3, k3) //12
